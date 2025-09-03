b3e161b75764abb340a956c85b06d11d8aca0e94
toprmrproducer <167063974+toprmrproducer@users.noreply.github.com>
2025-09-04 00:41:43 +0530

    Fix Clerk authentication completely

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ import './index.css'
 const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
 
 if (!PUBLISHABLE_KEY) {
-  console.error("Missing VITE_CLERK_PUBLISHABLE_KEY in .env file")
+  throw new Error("Missing Publishable Key")
 }
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
